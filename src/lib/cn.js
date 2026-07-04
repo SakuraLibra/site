4c313a95a3ba21b2d7c01a3a9af355db1ecc3564
Sakura Zhao <chaosonqi@gmail.com>
2026-07-04 22:26:01 +0100

    1.0
    更新

--- src/lib/cn.js
+++
@@ -1,7 +0,0 @@
-import { clsx } from 'clsx'
-import { twMerge } from 'tailwind-merge'
-
-// Merge conditional class names and dedupe conflicting Tailwind utilities.
-export function cn(...inputs) {
-  return twMerge(clsx(inputs))
-}
